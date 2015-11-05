84dbc025efaefa2ab9488cceedce399f9f7ee04b
Noah Martin-Ruben <nmartinr@umich.edu>
2015-11-05 00:59:21 -0500

    Added UnitBehavior, changed Person to implement UnitBehavior
    Added UnitBehavior.cs
    Person is now a child class of UnitBehavior
    Added some example code of how to use the new features
    TODO: Must still implement random movement
    No major changes made to Person, all still compiles

--- Assets/scripts/Person.cs
+++ Assets/scripts/Person.cs
@@ -1,49 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Person : MonoBehaviour {
-
-    public bool isAlive = true;
-    public float speed;
-    public int health = 100;
-    public float foodLevel = 100;
-    public float waterLevel = 100;
-
-    private GameObject waypoint = null;
-
+public class Person : UnitBehavior {
+	
+	public bool isAlive = true;
+	public float speed;
+	public int health = 100;
+	public float foodLevel = 100;
+	public float waterLevel = 100;
+	
+	private GameObject waypoint = null;
+	
 	// Use this for initialization
 	void Start () {
-	    
+		// Change base class variables here before calling initialize
+		base.updateTime = 2.0f;
+		// Call initialize to call necessary base class functions
+		base.initialize ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(waypoint != null)
-        {
-            Vector3 targetPoint = new Vector3(waypoint.transform.position.x, 0.5f, waypoint.transform.position.z);
-            transform.position = Vector3.MoveTowards(transform.position, targetPoint, Time.deltaTime * speed);
-            if (Mathf.Abs((transform.position - waypoint.transform.position).magnitude) < 0.1)
-                waypoint = null;
-        } else
-        {
-            waypoint = null;
-        }
+		if(waypoint != null)
+		{
+			Vector3 targetPoint = new Vector3(waypoint.transform.position.x, 0.5f, waypoint.transform.position.z);
+			transform.position = Vector3.MoveTowards(transform.position, targetPoint, Time.deltaTime * speed);
+			if (Mathf.Abs((transform.position - waypoint.transform.position).magnitude) < 0.1)
+				waypoint = null;
+		} else
+		{
+			waypoint = null;
+		}
+		
+		base.act();
 	}
-
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Consumable"))
-            Destroy(other.gameObject);
-    }
-
-    public void setWaypoint(GameObject waypoint)
-    {
-        this.waypoint = waypoint;
-    }
-
-    //TODO fix this to work with new waypoint system
-    public bool hasWaypoint()
-    {
-        return waypoint != null;
-    }
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Consumable"))
+			Destroy(other.gameObject);
+	}
+	
+	public void setWaypoint(GameObject waypoint)
+	{
+		this.waypoint = waypoint;
+	}
+	
+	//TODO fix this to work with new waypoint system
+	public bool hasWaypoint()
+	{
+		return waypoint != null;
+	}
+	
+	protected override void randomMove() {
+		//TODO at this point a new waypoint should be added in a random direction not too far away from the person
+		// I'm not sure how to do this, so I'm leaving it blank for now.
+		//print("subclass move");
+	}
 }
