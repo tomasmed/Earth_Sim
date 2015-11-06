1cde05ff6e7fb176a7b48698c461460c465d9194
Noah Martin-Ruben <nmartinr@umich.edu>
2015-11-05 19:12:02 -0500

    Changed person's waypoint to a vector3
    Person’s waypoint is now a Vector3, changed all implementation to make
    it work. A null value is now Vector3.zero, which could cause some
    problems in the future. To test whether a unit is moving or not, use
    person.getState() == State.MOVING.

--- Assets/scripts/Person.cs
+++ Assets/scripts/Person.cs
@@ -9,7 +9,7 @@ public class Person : UnitBehavior {
 	public float foodLevel = 100;
 	public float waterLevel = 100;
 	
-	private GameObject waypoint = null;
+	private Vector3 waypoint = Vector3.zero;
 	
 	// Use this for initialization
 	void Start () {
@@ -21,21 +21,15 @@ public class Person : UnitBehavior {
 	
 	// Update is called once per frame
 	void Update () {
-		if(waypoint != null)
-		{
-			unitState = State.MOVING;
-			Vector3 targetPoint = new Vector3(waypoint.transform.position.x, 0.5f, waypoint.transform.position.z);
-			transform.position = Vector3.MoveTowards(transform.position, targetPoint, Time.deltaTime * speed);
-			if (Mathf.Abs((transform.position - waypoint.transform.position).magnitude) < 0.1)
-			{
-				waypoint = null;
-				unitState = State.IDLE;
-			}
-		} else //TODO I believe this else clause is unnecessary. It's corresponding if checks to see if waypoint is not null, so there's no need to set waypoint to null again
-		{
-			waypoint = null;
-		}
-		
+		if (waypoint != Vector3.zero) {
+						unitState = State.MOVING;
+						Vector3 targetPoint = new Vector3 (waypoint.x, 0.5f, waypoint.z);
+						transform.position = Vector3.MoveTowards (transform.position, targetPoint, Time.deltaTime * speed);
+						if (Mathf.Abs ((transform.position - waypoint).magnitude) < 0.1) {
+								waypoint = Vector3.zero;
+								unitState = State.IDLE;
+						}
+				}
 		base.act();
 	}
 	
@@ -47,23 +41,27 @@ public class Person : UnitBehavior {
 	
 	public void setWaypoint(GameObject waypoint)
 	{
+		this.waypoint = waypoint.transform.position;
+	}
+
+	public void setWaypoint(Vector3 waypoint)
+	{
 		this.waypoint = waypoint;
 	}
 	
-	//TODO fix this to work with new waypoint system
+
 	public bool hasWaypoint()
 	{
-		return waypoint != null;
+		return waypoint != Vector3.zero;
 	}
 
 	// Creates a new waypoint GameObject with a random position relatively close to the current position
 	// Uses this GameObject to call setWaypoint
 	protected override void randomMove() {
-		GameObject newWaypoint = new GameObject(); //TODO For some reason this GameObject is not being deleted when player reaches waypoint
-		newWaypoint.transform.position = new Vector3(this.transform.position.x + (Random.value * 20) - 10,
+		Vector3 newWaypoint = new Vector3();
+		newWaypoint = new Vector3(this.transform.position.x + (Random.value * 20) - 10,
 		                                             this.transform.position.y,
 		                                             this.transform.position.z + (Random.value * 20) - 10);
-		setWaypoint(newWaypoint); // Can also be this.waypoint = newWaypoint
-		//UnityEngine.Object.Destroy(newWaypoint);
+		setWaypoint(newWaypoint);
 	}
 }
