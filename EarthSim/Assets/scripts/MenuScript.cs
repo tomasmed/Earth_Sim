f7557832563273e646eeb69ae870ea3ead7d6052
Noah Ehmann <nehmann@umich.edu>
2015-11-16 16:49:23 -0500

    Created basic menu and game over screens

---
+++ EarthSim/Assets/scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void StartGame() {
+		Application.LoadLevel("Test scene 1");
+	}
+
+	public void BackToMenu() {
+		Application.LoadLevel("TitleMenu");
+	}
+
+	public void QuitGame() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
